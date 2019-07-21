2d700dcd6929dfbef29aadf233e6d923a70c8284
lealfelipealves <lealfelipealves@gmail.com>
2019-07-20 21:20:45 -0300

    Atualizado o request de Contatos.

--- app/Http/Controllers/ContatosController.php
+++ app/Http/Controllers/ContatosController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\ContatosRequest as Request;
 use App\Models\Contato;
 
 class ContatosController extends Controller
