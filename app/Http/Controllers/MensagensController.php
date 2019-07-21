bc185bcfd3f3df7328f8d88a2e4586a168dc60e0
lealfelipealves <lealfelipealves@gmail.com>
2019-07-20 21:25:35 -0300

    Adicionado MensagemRequest no controller.

--- app/Http/Controllers/MensagensController.php
+++ app/Http/Controllers/MensagensController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\MensagensRequest as Request;
+use App\Models\Mensagem;
 
 class MensagensController extends Controller
 {
