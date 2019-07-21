5d9a2c4a3c08320ca314d345c6d38c1473e12eb5
lealfelipealves <lealfelipealves@gmail.com>
2019-07-20 21:11:19 -0300

    Criado o controller Mensagem(Criar, listar, alterar, deletar).

---
+++ app/Http/Controllers/MensagensController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MensagensController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Mensagem[]
+     */
+    public function index()
+    {
+        return Mensagem::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Mensagem
+     */
+    public function store(Request $request)
+    {
+        return Mensagem::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Mensagem  $mensagem
+     * @return Mensagem
+     */
+    public function show(Mensagem $mensagem)
+    {
+        return $mensagem;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Mensagem  $mensagem
+     * @return Mensagem
+     */
+    public function update(Request $request, Mensagem $mensagem)
+    {
+        $mensagem->update($request->all());
+        return $mensagem;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Mensagem  $mensagem
+     * @return Mensagem
+     */
+    public function destroy(Request $request, Mensagem $mensagem)
+    {
+        $mensagem->delete();
+        return $mensagem;
+    }
+}
