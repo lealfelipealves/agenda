1f9ed23e6537605b15a64902f156caa44b634d86
lealfelipealves <lealfelipealves@gmail.com>
2019-07-20 21:09:36 -0300

    Criado o controller Contato(Criar, Alterar, Excluir).

---
+++ app/Http/Controllers/ContatosController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contato;
+
+class ContatosController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Contato
+     */
+    public function store(Request $request)
+    {
+        return Contato::create($request->all());
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Contato  $contato
+     * @return Contato
+     */
+    public function update(Request $request, Contato $contato)
+    {
+        $contato->update($request->all());
+        return $contato;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Contato  $contato
+     * @return Contato
+     */
+    public function destroy(Request $request, Contato $contato)
+    {
+        $contato->delete();
+        return $contato;
+    }
+}
